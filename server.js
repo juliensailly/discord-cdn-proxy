97f938d208b39aafb7b6e1b09203b97c9b4176e5
Julien Sailly <juliensailly76@gmail.com>
2024-10-24 17:56:19 +0200

    feat: beginning of the server

---
+++ server.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const https = require('https');
+const url = require('url');
+
+function fetchContent(targetUrl, res) {
+    const parsedUrl = url.parse(targetUrl);
+
+    const protocol = parsedUrl.protocol === 'https:' ? https : http;
+
+    protocol.get(targetUrl, (proxyRes) => {
+        let data = '';
+
+        proxyRes.on('data', (chunk) => {
+            chu
+            data += chunk;
+        });
+
+        proxyRes.on('end', () => {
+            res.writeHead(proxyRes.statusCode, proxyRes.headers);
+            res.end(data);
+        });
+
+    }).on('error', (err) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('An error occured: ' + err.message);
+    });
+}
+
+http.createServer((req, res) => {
+    const queryObject = url.parse(req.url, true).query;
+
+    if (queryObject.url) {
+        const targetUrl = queryObject.url;
+
+        if (targetUrl.startsWith('http://') || targetUrl.startsWith('https://')) {
+            fetchContent(targetUrl, res);
+        } else {
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('URL not valid. Only HTTP and HTTPS protocols are supported.');
+        }
+    } else {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('Please enter a valid URL using the param ?url=');
+    }
+}).listen(80, () => {
+    console.log('Proxy server started on port 80');
+});
