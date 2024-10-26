29e26298f0360c4ec3c78f1ec50d21eda5a75157
Julien Sailly <juliensailly76@gmail.com>
2024-10-26 12:58:56 +0200

    feat: improving server.js

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-const http = require('http');
-const https = require('https');
-const url = require('url');
+const http = require("http");
+const https = require("https");
+const url = require("url");
 
 function fetchContent(targetUrl, res) {
     const parsedUrl = url.parse(targetUrl);
 
-    const protocol = parsedUrl.protocol === 'https:' ? https : http;
+    const protocol = parsedUrl.protocol === "https:" ? https : http;
 
-    protocol.get(targetUrl, (proxyRes) => {
-        let data = '';
+    protocol
+        .get(targetUrl, (proxyRes) => {
+            let data = "";
 
-        proxyRes.on('data', (chunk) => {
-            chu
-            data += chunk;
+            proxyRes.on("data", (chunk) => {
+                data += chunk;
+                data = data.replace('="/', '="/?url=/');
+                console.log(data);
+            });
+
+            proxyRes.on("end", () => {
+                res.writeHead(proxyRes.statusCode, proxyRes.headers);
+                res.end(data);
+            });
+        })
+        .on("error", (err) => {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Error fetching content");
         });
-
-        proxyRes.on('end', () => {
-            res.writeHead(proxyRes.statusCode, proxyRes.headers);
-            res.end(data);
-        });
-
-    }).on('error', (err) => {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('An error occured: ' + err.message);
-    });
 }
 
 http.createServer((req, res) => {
@@ -32,16 +34,21 @@ http.createServer((req, res) => {
     if (queryObject.url) {
         const targetUrl = queryObject.url;
 
-        if (targetUrl.startsWith('http://') || targetUrl.startsWith('https://')) {
+        if (
+            targetUrl.startsWith("http://") ||
+            targetUrl.startsWith("https://")
+        ) {
             fetchContent(targetUrl, res);
         } else {
-            res.writeHead(400, { 'Content-Type': 'text/plain' });
-            res.end('URL not valid. Only HTTP and HTTPS protocols are supported.');
+            res.writeHead(400, { "Content-Type": "text/plain" });
+            res.end(
+                "URL not valid. Only HTTP and HTTPS protocols are supported."
+            );
         }
     } else {
-        res.writeHead(400, { 'Content-Type': 'text/plain' });
-        res.end('Please enter a valid URL using the param ?url=');
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        res.end("Please enter a valid URL using the param ?url=");
     }
 }).listen(80, () => {
-    console.log('Proxy server started on port 80');
+    console.log("Proxy server started on port 80");
 });
