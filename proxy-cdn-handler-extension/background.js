2ffbad365e4525d1c47bb238054232cbadcdc6cf
Julien Sailly <julien.sailly@sap.com>
2024-10-31 11:01:33 +0100

    feat: firefox extension

--- proxy-cdn-handler-extension/background.js
+++ proxy-cdn-handler-extension/background.js
@@ -1,7 +1,17 @@
-chrome.webRequest.onBeforeRequest.addListener(
+// Fonction pour encoder l'URL
+function encodeUrl(url) {
+    return encodeURIComponent(url);
+}
+
+// Intercepter les requêtes et rediriger
+browser.webRequest.onBeforeRequest.addListener(
     (details) => {
-        const originalUrl = encodeURIComponent(details.url);
-        const proxyUrl = `http://discord-cdn-proxy.juliensailly.com/?url=${originalUrl}`;
+        const originalUrl = details.url;
+        const encodedUrl = encodeUrl(originalUrl);
+
+        // Nouvelle URL avec le proxy
+        const proxyUrl = `http://discord-cdn-proxy.juliensailly.com?url=${encodedUrl}`;
+
         return { redirectUrl: proxyUrl };
     },
     { urls: ["*://cdn.discordapp.com/*"] },
